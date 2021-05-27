739cd8d3af8a604c73450379de245f11828a2bec
Levi Zitting <me@levizitting.com>
2021-05-26 22:11:09 -0500

    Add automatic api docs to controllers project

--- QuipVidControllers/Requests/CreateMediaRequest.cs
+++ QuipVidControllers/Requests/CreateMediaRequest.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace QuipVidControllers.Requests
 {
     public class CreateMediaRequest
     {
+        [Required]
         public string Title { get; set; }
     }
 }
--- QuipVidControllers/Requests/CreateQuipRequest.cs
+++ QuipVidControllers/Requests/CreateQuipRequest.cs
@@ -1,13 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuipVidControllers.Requests
 {
     public class CreateQuipRequest
     {
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string VideoUrl { get; set; }
+
+        [Required]
         public string ThumbnailUrl { get; set; }
+
+        [Required]
         public Guid MediaId { get; set; }
+
+        [Required]
         public Guid UploaderId { get; set; }
     }
 }
