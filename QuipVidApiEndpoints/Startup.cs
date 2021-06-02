e3691f6504a12b5ba0fab2cbcdbc4f54c4515e06
Levi Zitting <me@levizitting.com>
2021-06-02 17:23:56 -0500

    Fix error with swagger

--- QuipVidApiEndpoints/Startup.cs
+++ QuipVidApiEndpoints/Startup.cs
@@ -18,6 +18,10 @@ namespace QuipVidApiEndpoints
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddMvcCore()
+                .AddApiExplorer();
+
             services.AddAutoMapper(typeof(Startup));
 
             services.AddDbContext<AppDbContext>(opt =>
